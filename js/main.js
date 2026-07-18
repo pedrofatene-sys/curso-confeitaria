644429950039d5aaa27b83e0f5f5e057d17068b3
pedrofatene-sys <pedro.fatene@gmail.com>
2026-07-17 22:53:27 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Lista de vídeos do YouTube (IDs extraídos das URLs)
 const channels = [
-    { id: 'Jaz23nnvcRw', name: 'Massa Branca - Delícias da Cilinha ' },
+    { id: 'QunbMXCwU_U', name: 'Massa Branca - Delícias da Cilinha ' },
     { id: 'EuHDgkVliII', name: 'CONSTITUIÇÃO - CONCEITO E CLASSIFICAÇÕES - Parte 2' },
     { id: 't93FyKwCtjo', name: 'Princípios fundamentais: Princípios IMPLÍCITOS e EXPLÍCITOS' },
     { id: 'q_ZMMxTnrOE', name: 'Direitos e Garantias Fundamentais' },
